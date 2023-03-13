22b0917cb0a7cba25cedadd84dbdcef1d4a4e23c
tffaruk <faruk.themefisher@gmail.com>
2023-03-13 11:08:03 +0600

    add tab and modal

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -25,3 +25,47 @@
     },
   });
 })();
+
+// for tab component
+const tabs = document.querySelectorAll(".tabs>li");
+const tabElements = document.querySelectorAll(".tab-element");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    console.log(tabs);
+    // remove active class from all tabs and tab contents
+    tabs.forEach((tab) => tab.classList.remove("active-tab"));
+    tabElements.forEach((tabElement) =>
+      tabElement.classList.remove("active-tab")
+    );
+
+    // add active class to clicked tab and corresponding tab content
+    tab.classList.add("active-tab");
+    tabElements[index].classList.add("active-tab");
+  });
+});
+
+// modal compon
+// Get the modal elements
+const modal = document.getElementById("modal");
+const modalOverlay = document.getElementById("modal-container");
+const modalOpenBtn = document.getElementById("modal-open-button");
+const modalCloseBtn = document.getElementById("modal-close-button");
+
+// modal open button
+modalOpenBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modalOverlay.classList.remove("hidden");
+});
+
+// modal close button
+modalCloseBtn.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  modalOverlay.classList.add("hidden");
+});
+
+// Close the modal click on the modal container
+modalOverlay.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  modalOverlay.classList.add("hidden");
+});
